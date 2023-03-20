88b44370319970fdd19d2d26ea796fe23f436530
abdullahhgit <abdullahkhan42069@gmail.com>
2023-03-20 18:13:39 +0500

    API testing completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { store } from './store/store'
+import { Provider } from 'react-redux'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>
 )
