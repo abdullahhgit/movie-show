6822d8a52d03436873c23d67c7a259c4fd48af25
abdullahhgit <abdullahkhan42069@gmail.com>
2023-03-21 18:21:08 +0500

    start working on Redux tookit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import {useEffect} from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { fetchDataFromApi } from "./utils/api";
+import { getApiConfiguration } from './store/homeSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
 
   useEffect(() => {
     apiTesting();
@@ -11,12 +15,15 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {      // For API testing
       console.log(res); 
+      dispatch(getApiConfiguration(res));
     });
   }
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
   )
 }
 
--- src/store/homeSlice.js
+++ src/store/homeSlice.js
@@ -1,27 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { PayloadAction } from '@reduxjs/toolkit'
 
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+
+export const homeSlice = createSlice({
+  name: 'home',
+  initialState: {
+    url: {},
+    genres: {},
+  },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
+    getApiConfiguration: (state, action) => {
+      state.url = action.payload;
     },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+
+    getGenresConfiguration: (state, action) => {
+      state.genres = action.payload;
+    }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const {getApiConfiguration, getGenresConfiguration} = homeSlice.actions
 
-export default counterSlice.reducer
+export default homeSlice.reducer
