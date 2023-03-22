d3ebef268f2badd493deb514c3eaa1f6e8b34452
abdullahhgit <abdullahkhan42069@gmail.com>
2023-03-22 11:24:52 +0500

    Working on home banner

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,19 @@ function App() {
  console.log(`Iam url console ${url?.total_pages}`);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {      // For API testing
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {      // For API testing
       console.log(res); 
+
+   /*   const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      } */
+
       dispatch(getApiConfiguration(res));
     });
   }
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import UseFetch from '../../../hooks/UseFetch';
+import SearchResult from '../../searchResult/SearchResult';
 
 const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if(event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      <SearchResult />
+    }
+  }
+
+  const {data, loading} = UseFetch("/movie/upcoming");
+
+  useEffect(() => {
+    const bg = data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
   return (
-    <div>
-      Hero Banner Section
+    <div className='heroBanner'>
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover.
+            Explore now.
+          </span>
+          <div className="searchInput">
+            <input type="text" placeholder='Search for a movie or TV show'
+              onChange={(e) => setQuery(e.target.value)} 
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
