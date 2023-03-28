7bc3b92960c8d06974fff1ec4fc777f42bcb74e2
abdullahhgit <abdullahkhan42069@gmail.com>
2023-03-28 11:15:55 +0500

    Trending Section completed

---
+++ src/components/caurosel/Caurosel.jsx
@@ -0,0 +1,81 @@
+import React, { useRef } from "react";
+import {
+    BsFillArrowLeftCircleFill,
+    BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import {ContentWrapper} from "../contentWrapper/ContentWrapper";
+import {Img} from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+//import CircleRating from "../circleRating/CircleRating";
+//import Genres from "../genres/Genres";
+import "./style.scss";
+import CircleRating from "../circleRatings/CircleRating";
+
+const Caurosel = ({ data, loading}) => {
+    const carouselContainer = useRef();
+    const { url } = useSelector((state) => state.home);
+    const navigate = useNavigate();
+
+    const navigation = (dir) => {
+
+    }
+
+    return (
+        <div className="carousel">
+            <ContentWrapper>
+                <BsFillArrowLeftCircleFill
+                    className="carouselLeftNav arrow"
+                    onClick={() => navigation("left")}
+                />
+                <BsFillArrowRightCircleFill
+                    className="carouselRighttNav arrow"
+                    onClick={() => navigation("right")}
+                />
+                {!loading ? (
+                    <div className="carouselItems">
+                        {data?.map((item) => {
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="carouselItem"
+                                >
+                                    <div className="posterBlock">
+                                        <Img src={posterUrl} />
+
+                                        <CircleRating
+                                            rating={item.vote_average.toFixed(
+                                                1
+                                            )}
+                                        />
+                                    </div>
+
+                                    <div className="textBlock">
+                                        <span className="title">
+                                            {item.title || item.name}
+                                        </span>
+                                        <span className="date">
+                                            {dayjs(item.release_Date).format(
+                                                "MMM D, YYYY"
+                                            )}
+                                        </span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <span>Loading...</span>
+                )}
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default Caurosel
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Caurosel from '../../../components/caurosel/Caurosel'
 import {ContentWrapper} from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+import useFetch from '../../../hooks/UseFetch'
 
 const Trending = () => {
-    const onTabChange = () => {
-
+    const [endPoint, setEndpoint] = useState('day');
+    const {data, loading} = useFetch(`/trending/all/${endPoint}`)
+    
+    const onTabChange = (tab) => {
+      setEndpoint(tab==='Day' ? 'day' : 'week');
     }
   return (
     <div className='cauroselSection'>
@@ -14,6 +19,7 @@ const Trending = () => {
         </span>
         <SwitchTabs data={["Day", "Week"]} onTabChange = {onTabChange} />
       </ContentWrapper>
+      <Caurosel data={data?.results} loading={loading} />
     </div>
   )
 }
